bdc67a01f82bcbde10b757c5f9288df9ef7f3637
yanglei <lei.yang@gi-de.com>
2018-06-12 15:16:43 +0800

    first version

---
+++ js/resize.js
@@ -0,0 +1,54 @@
+// var $el = $("#resize");
+// var elHeight = $el.outerHeight();
+// var elWidth = $el.outerWidth();
+
+// $(window).resize(function () {
+//   doResize();
+// });
+
+// // var $wrapper = $(window);
+
+// // $wrapper.resizable({
+// //   resize: doResize
+// // });
+
+// function doResize(event, ui) {
+  
+//   var scale, origin;
+    
+//   scale = Math.min(
+//     window.screen.availWidth / elWidth,    
+//     window.screen.availHeight / elHeight
+//   );
+  
+//   $el.css({
+//     transform: "translate(-50%, -50%) " + "scale(" + scale + ")"
+//   });
+  
+// }
+
+// var starterData = { 
+//   size: {
+//     width: window.screen.availWidth,
+//     height: window.screen.availHeight
+//   }
+// }
+//doResize(null, starterData);
+
+function doResize(event, ui) {
+  
+  var $el = $("#resize");
+  var elHeight = $el.outerHeight();
+  var elWidth = $el.outerWidth();
+  var scale, origin;
+  scale = window.screen.availWidth / elWidth
+
+  $el.css({
+    transform: "translate(-50%, -50%) " + "scale(" + scale + ")"
+  });
+  
+}
+
+$( document ).ready(function() {
+  doResize()
+});
